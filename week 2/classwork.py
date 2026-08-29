6092f5dd5693c77a84fc9f2cda2abeac500ce491
Alison Voisin <avoisin0818@icloud.com>
2026-08-28 19:33:45 -0500

    week 2/classwork.py

---
+++ week 2/classwork.py
@@ -0,0 +1,20 @@
+x = input("enter your name")
+score = float(input("enter your score"))
+print(type(x))
+print(type(score))
+print("Hello " + x + ", your score is" + score)
+
+print(float(score) + 5) 
+
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+total= num1 + num2
+print("the total is", total)
+
+
+#this code is for collecting data
+#this code is for collecting data
+# This code is for collecting data
+x= input("enter your name")
+x= input("enter your age")
+x = input("enter your score") #dont forget to change to float
---
+++ week 2/lab1.py
@@ -0,0 +1,41 @@
+'''
+x = input("enter your first name")
+y = input("enter your last name")
+print (y + ", " + x)'''
+
+
+'''
+x = 32
+y = "sara"
+z = 3.5
+print(type(x))
+print(type(y))
+print(type(z))'''
+
+'''
+print("A\nB\nC\nD\nE")'''
+
+'''
+x = "C:\\Users\\rick\\Documents"
+print(x)
+y = ''' --- SYSTEM MENU ---
+1. Open File
+2. Save File'''
+print(y)
+z = 'The professor said, \'Make sure you escape matching quotes!\''
+print(z) '''
+
+
+'''
+l=float(input("enter the length of the rectangle"))
+w =float(input("enter the width of the rectangle"))
+A = l * w
+print("the area of the rectangle is", A)'''
+
+x = input("enter your name")
+y = float(input ("enter monthly budget"))
+z = float(input("enter daily food cost"))
+monthly_food_cost = z * 30
+remaining_budget = y - monthly_food_cost
+print("---BUDGET SUMMARY---")
+print(x,"| Food:", monthly_food_cost, "| Remaining Budget:", remaining_budget)
