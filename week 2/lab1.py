b8373a918dcd9cf958ca24e867f7c891462f2f69
Alison Voisin <avoisin0818@icloud.com>
2026-09-03 11:02:19 -0500

    week 3/lab2

--- week 2/lab1.py
+++ week 2/lab1.py
@@ -1,36 +1,37 @@
 '''
 x = input("enter your first name")
 y = input("enter your last name")
-print (y + ", " + x)'''
+print (y + ", " + x)
 
 
-'''
+
 x = 32
 y = "sara"
 z = 3.5
 print(type(x))
 print(type(y))
-print(type(z))'''
+print(type(z))
 
-'''
-print("A\nB\nC\nD\nE")'''
+
+print("A\nB\nC\nD\nE")
 
 
 x = "C:\\Users\\rick\\Documents"
 print(x)
-y = ''' --- SYSTEM MENU ---
+y =  --- SYSTEM MENU ---
 1. Open File
-2. Save File'''
+2. Save File
 print(y)
 z = 'The professor said, \'Make sure you escape matching quotes!\''
-print(z) 
+print(z)
 
 
-'''
+
 l=float(input("enter the length of the rectangle"))
 w =float(input("enter the width of the rectangle"))
 A = l * w
-print("the area of the rectangle is", A)'''
+print("the area of the rectangle is", A)
+
 
 x = input("enter your name")
 y = float(input ("enter monthly budget"))
@@ -39,3 +40,34 @@ monthly_food_cost = z * 30
 remaining_budget = y - monthly_food_cost
 print("---BUDGET SUMMARY---")
 print(x,"| Food:", monthly_food_cost, "| Remaining Budget:", remaining_budget)
+
+pi= 3.14
+r= float(input("enter the radius of the circle"))
+A=pi * r**2
+print(A)
+
+C=float(input("enter temp in celsius"))
+F= (C * (9/5) + 32)
+print(F)
+
+n1 = float(input("enter the first number"))
+n2 = float(input("enter the second number"))
+n3 = float(input("enter the third number"))
+average = (n1+ n2+ n3) / 3
+print(average)
+
+x=10
+y=20
+temp=x
+x=y
+y=temp
+
+op=float(input("enter original price"))
+dp=float(input("enter discount percentage"))
+tax=float(input("enter tax percentage"))
+discont_amount= op * (dp/100)
+price_after_discount= op - discont_amount
+tax_amount= price_after_discount * tax/100
+final_price= price_after_discount - tax_amount
+print(final_price)'''
+
